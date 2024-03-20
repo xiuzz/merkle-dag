76c0bca729dfb2dedc2ea39dd064e7ded1dfea3e
xiuzz <1519072696@qq.com>
2024-03-20 19:11:28 +0800

    modify to tidy

--- dag_test.go
+++ dag_test.go
@@ -8,102 +8,6 @@ import (
 	"testing"
 )
 
-type TestFile struct {
-	name string
-	data []byte
-}
-
-func (file *TestFile) Size() uint64 {
-	return uint64(len(file.data))
-}
-
-func (file *TestFile) Name() string {
-	return file.name
-}
-
-func (file *TestFile) Type() int {
-	return FILE
-}
-
-func (file *TestFile) Bytes() []byte {
-	return file.data
-}
-
-type testDirIter struct {
-	list []Node
-	iter int
-}
-
-func (iter *testDirIter) Next() bool {
-	if iter.iter+1 < len(iter.list) {
-		iter.iter += 1
-		return true
-	}
-	return false
-}
-
-func (iter *testDirIter) Node() Node {
-	return iter.list[iter.iter]
-}
-
-type TestDir struct {
-	list []Node
-	name string
-}
-
-func (dir *TestDir) Size() uint64 {
-	var len uint64 = 0
-	for i := range dir.list {
-		len += dir.list[i].Size()
-	}
-	return len
-}
-
-func (dir *TestDir) Name() string {
-	return dir.name
-}
-
-func (dir *TestDir) Type() int {
-	return DIR
-}
-
-func (dir *TestDir) It() DirIterator {
-	it := &testDirIter{
-		list: dir.list,
-		iter: -1,
-	}
-	return it
-}
-
-type HashMap struct {
-	mp map[string]([]byte)
-}
-
-func (hmp *HashMap) Has(key []byte) (bool, error) {
-	return hmp.mp[string(key)] != nil, nil
-}
-
-func (hmp *HashMap) Put(key, value []byte) error {
-	flag, _ := hmp.Has(key)
-	if flag {
-		panic("Key is same")
-	}
-	hmp.mp[string(key)] = value
-	return nil
-}
-
-func (hmp *HashMap) Get(key []byte) ([]byte, error) {
-	flag, _ := hmp.Has(key)
-	if !flag {
-		panic("Don't have the key")
-	}
-	return hmp.mp[string(key)], nil
-}
-
-func (hmp *HashMap) Delete(key []byte) error {
-	return nil
-}
-
 func TestDag(t *testing.T) {
 	kv := &HashMap{
 		mp: make(map[string][]byte),
@@ -164,6 +68,56 @@ func TestDag(t *testing.T) {
 	}
 	root = Add(kv, dir, h)
 	fmt.Printf("%x\n", root)
+}
+
+func TestDag2file(t *testing.T) {
+	kv := &HashMap{
+		mp: make(map[string][]byte),
+	}
+	h := sha256.New()
+	// a folder
+	kv = &HashMap{
+		mp: make(map[string][]byte),
+	}
+	path := "/home/xiuuix/go"
+	files, _ := ioutil.ReadDir(path)
+	dir := &TestDir{
+		list: make([]Node, len(files)),
+		name: "/",
+	}
+	for i, fi := range files {
+		newPath := path + "/" + fi.Name()
+		if fi.IsDir() {
+			context := search(newPath)
+			context.name = fi.Name()
+			dir.list[i] = context
+		} else {
+			context, err := os.ReadFile(newPath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			file := &TestFile{
+				name: fi.Name(),
+				data: context,
+			}
+			dir.list[i] = file
+		}
+	}
+	root := Add(kv, dir, h)
+	fmt.Printf("%x\n", root)
+	buffer_go := Hash2File(kv, root, "/pkg/mod/bazil.org/fuse@v0.0.0-20200117225306-7b5117fecadc/buffer.go", nil)
+	fmt.Println(string(buffer_go))
+	dlv := Hash2File(kv, root, "/bin/dlv", nil)
+	context, _ := os.ReadFile("/home/xiuuix/go/bin/dlv")
+	h.Reset()
+	h.Write(context)
+	tmp1 := h.Sum(nil)
+	h.Reset()
+	h.Write(dlv)
+	tmp2 := h.Sum(nil)
+	fmt.Println(tmp1)
+	fmt.Println(tmp2)
+	fmt.Println(string(tmp1) == string(tmp2))
 }
 
 func search(path string) *TestDir {
